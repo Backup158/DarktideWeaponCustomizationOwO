f686a26e5063009f54658fa5c7d638241da10358
Backup158 <beekoop158@gmail.com>
2025-02-28 09:35:30 -0600

    removed personal edits from this plugin (they have their own home now)

--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/combatknife_p1_m1.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/combatknife_p1_m1.lua
@@ -26,6 +26,7 @@ local this_variant = "combatknife_p1_m1" -- Combat Blade
 -- ############################################
 mod.mt.inject_fixes(this_variant, {
 
+    --[[
     {   dependencies = {"ogrynbladehandle_01",
             --"knife_body_01|knife_body_02|knife_body_03|knife_body_04|knife_body_05|knife_body_06|knife_body_07|combat_knife_blade_ml01"
             "knife_body_03"
@@ -33,5 +34,6 @@ mod.mt.inject_fixes(this_variant, {
         grip = {mesh_move = true, position = vector3_box(0, 0, -0.01), rotation = vector3_box(-1, 0, 0), scale = vector3_box(.35, .35, .45), node = 1},
         trinket_hook = {position = vector3_box(0, 0, -.064), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.834, 0.834, 0.834)},    
         body = {position = vector3_box(0, 0, .06), rotation = vector3_box(0, 0, 0), scale = vector3_box(1, 1, 1)}
-    },    
+    }, 
+    ]]   
 })
--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/forcesword_2h_p1_m1.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/forcesword_2h_p1_m1.lua
@@ -25,6 +25,7 @@ local this_variant = "forcesword_2h_p1_m1" -- Force Greatsword
 -- Inject Fixes
 -- ############################################
 mod.mt.inject_fixes(this_variant, {
+    --[[
     {   dependencies = {"2h_force_sword_hilt_01",
             "power_sword_grip_04",
             "2h_power_maul_pommel_03",
@@ -34,5 +35,6 @@ mod.mt.inject_fixes(this_variant, {
         hilt =      { position = vector3_box(0, 0, -0.008), rotation = vector3_box(0, 0, 0), scale = vector3_box(1, 1, 0.76)}, 
         grip =      { position = vector3_box(0, 0, 0.0), rotation = vector3_box(0, 0, 0), scale = vector3_box(1, 1, 1)},
         pommel =    { position = vector3_box(0, 0, -0.176), rotation = vector3_box(0, 0, 0), scale = vector3_box(1, 1, 1)},
-    },    
+    }, 
+    ]]   
 })
