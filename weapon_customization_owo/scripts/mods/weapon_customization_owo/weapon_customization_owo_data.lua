86de0659c52d1f642369f702b0db3ca9a36150d4
Backup158 <beekoop158@gmail.com>
2025-04-15 21:34:49 -0500

    feat: Localization options

--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon_customization_owo_data.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon_customization_owo_data.lua
@@ -1,7 +1,26 @@
 local mod = get_mod("weapon_customization_owo")
 
+local finalWidgets = {}
+local widgetsToggleableToAdd = {"debug_mode", "owo_mode"}
+-- Appends a toggleable option for a new widget
+local function addWidget(name)
+    -- Write at (table size) + 1, ie inserting at the tail
+    finalWidgets[#finalWidgets + 1] = {
+        setting_id = name,
+        type = "checkbox",
+        default_value = false,
+    }
+end
+-- Adds a widget for each one in the list of names
+for _, name in pairs(widgetsToggleableToAdd) do
+    addWidget(name)
+end
+
 return {
 	name = mod:localize("mod_name"),
 	description = mod:localize("mod_description"),
 	is_togglable = true,
+	options = {
+		widgets = finalWidgets
+	}
 }
--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon_customization_owo_localization.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon_customization_owo_localization.lua
@@ -1,6 +1,6 @@
 local mod = get_mod("weapon_customization_owo")
 
-return {
+local localizations = {
 	mod_name = {
 		en = "Weapon Customization - OwO (Ostracized without Objection)",
 		["zh-cn"] = "武器自定义扩展 - OwO（Ostracized without Objection）",
@@ -10,7 +10,31 @@ return {
 	mod_description = {
 		en = "Weapon Customization plugin adding various parts, typically inspired by historical and tacticool weapons.",
 		["zh-cn"] = "为武器自定义扩展插件添加各种附件,这些附件受史实武器及tacticool启发",
-		-- ru = "У меня в арсенале всего одна маленькая анальная пробка, так что мне явно нужно еще много времени, чтобы научиться комфортно принимать член",
 		ru = "Если кто-то присылает вам «UwU», не ломайте себе голову в поиске ответа, пытаясь понять, что вообще это означает. Это слово нельзя расшифровать по буквам. Вместо этого термин используется, как эмодзи для обозначения реакции на что-то милое или радующее вас. Представьте картину милого котёнка, который залез с лапками в блюдце с молоком. Это так мило, что почти отвратительно, и ваша первая реакция, скорее всего, «Ой! Как восхитительно» или «UwU», что произносится как «ooh woo».",
 	},
+	debug_mode = {
+		en = "Debug Mode",
+	},
+	debug_mode_description = {
+		en = "Verbose printing",
+	},
+	owo_mode = {
+		en = "OwO Mode",
+	},
+	owo_mode_description = {
+		en = "UwUified descriptions on the mod options page >:3 Requires a game restart/reload to apply.",
+	},
 }
+
+--[[if mod:get("owo_mode") then
+	localizations[mod_name][en] = "Weapon Customization - OwO (O-ostwacized *runs away* without Objection >w<)"
+	localizations[mod_description][en] = "Weapon Customization plugin adding vawious pawts, typicawwy inspiwed >w< by *whispers to self* histowicaw and tacticoow weapons."
+	localizations[mod_description][ru] = "У меня в арсенале всего одна маленькая анальная пробка, так что мне явно нужно еще много времени, чтобы научиться комфортно принимать член"
+	localizations[debug_mode][en] = "Debug M-m-mode *walks away*"
+	localizations[debug_mode_description][en] = "V-Vewbose pwinting"
+	localizations[owo_mode][en] = "OwO Mode"
+	localizations[owo_mode_description][en] = "UwUified ;;w;; descwiptions on the x3 mod options *whispers to self* page *twerks* >:3 Wequiwes a game westawt/wewoad (・`ω´・) to *looks at you* apply."
+end]]
+
+return localizations
+
