dccb125ca9da31884d67c7e26a43a35edfd41e73
Backup158 <beekoop158@gmail.com>
2025-05-09 15:52:51 -0500

    style: renamed variables for new name

--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/powersword_p1_m1.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/powersword_p1_m1.lua
@@ -31,8 +31,8 @@ mod.owo_slim_blade(this_variant)
 -- ############################################
 -- Inject Fixes
 -- ############################################
-local _owo_slim_psword_blades = "owo_slim_psword_01|owo_slim_psword_02|owo_slim_psword_03|owo_slim_psword_04|owo_slim_psword_05|owo_slim_psword_06|owo_slim_psword_07|owo_slim_psword_08"
-local _owo_thin_psword_blades = "owo_slim_psword_01t|owo_slim_psword_02t|owo_slim_psword_03t|owo_slim_psword_04t|owo_slim_psword_05t|owo_slim_psword_06t|owo_slim_psword_07t|owo_slim_psword_08t"
+local _owo_flat_psword_blades = "owo_slim_psword_01|owo_slim_psword_02|owo_slim_psword_03|owo_slim_psword_04|owo_slim_psword_05|owo_slim_psword_06|owo_slim_psword_07|owo_slim_psword_08"
+local _owo_slim_psword_blades = "owo_slim_psword_01t|owo_slim_psword_02t|owo_slim_psword_03t|owo_slim_psword_04t|owo_slim_psword_05t|owo_slim_psword_06t|owo_slim_psword_07t|owo_slim_psword_08t"
 local _owo_slim_dclaw_blades = "owo_slim_dclaw_01|owo_slim_dclaw_02|owo_slim_dclaw_03|owo_slim_dclaw_04|owo_slim_dclaw_05|owo_slim_dclaw_06|owo_slim_dclaw_07|owo_slim_dclaw_08"
 local _owo_slim_dclaw_g_blades = "owo_slim_dclaw_01g|owo_slim_dclaw_02g|owo_slim_dclaw_03g|owo_slim_dclaw_04g|owo_slim_dclaw_05g|owo_slim_dclaw_06g|owo_slim_dclaw_07g|owo_slim_dclaw_08g"
 local _owo_slim_all_dclaw_blades = _owo_slim_dclaw_blades.."|".._owo_slim_dclaw_g_blades
@@ -40,21 +40,21 @@ local _owo_slim_all_dclaw_blades = _owo_slim_dclaw_blades.."|".._owo_slim_dclaw_
 local _power_sword_grips = "power_sword_grip_01|power_sword_grip_02|power_sword_grip_03|power_sword_grip_04|power_sword_grip_05|power_sword_grip_06"
 
 mod.mt.inject_fixes(this_variant, {
+    --  Flat Power Sword
+    --      Hilt Patches
+    {   dependencies =  { _owo_flat_psword_blades, _power_sword_grips },
+        hilt = {position = vector3_box(0, 0, 0.06), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
+    },
+    {   dependencies =  { _owo_flat_psword_blades },
+        hilt = {position = vector3_box(0, 0, 0.02), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
+        blade = {position = vector3_box(0, 0, -0.022), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.6, 1, 1)},
+    },
     --  Slim Power Sword
     --      Hilt Patches
     {   dependencies =  { _owo_slim_psword_blades, _power_sword_grips },
         hilt = {position = vector3_box(0, 0, 0.06), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
     },
     {   dependencies =  { _owo_slim_psword_blades },
-        hilt = {position = vector3_box(0, 0, 0.02), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
-        blade = {position = vector3_box(0, 0, -0.022), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.6, 1, 1)},
-    },
-    --  Thin Power Sword
-    --      Hilt Patches
-    {   dependencies =  { _owo_thin_psword_blades, _power_sword_grips },
-        hilt = {position = vector3_box(0, 0, 0.06), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
-    },
-    {   dependencies =  { _owo_thin_psword_blades },
         hilt = {position = vector3_box(0, 0, 0.02), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.5, 1, 1)},
         blade = {position = vector3_box(0, 0, -0.022), rotation = vector3_box(0, 0, 0), scale = vector3_box(0.6, 0.65, 1)},
     },
