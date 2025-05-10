e5f8349a4d20d1a2059ef1d46692e0e943b5eec1
Backup158 <beekoop158@gmail.com>
2025-05-10 02:23:45 -0500

    refactor: rapier variables for dueling sword

--- weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/combatsword_p3_m1.lua
+++ weapon_customization_owo/scripts/mods/weapon_customization_owo/weapon/combatsword_p3_m1.lua
@@ -24,6 +24,10 @@ mod.owo_rapier_blade(this_variant)
 -- ############################################
 -- Define Variables for Fixes
 -- ############################################
+local _mt_knife_grips = "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
+--local _mt_falchion_grips = "falchion_mt_grip_01|falchion_mt_grip_02|falchion_mt_grip_03|falchion_mt_grip_04|falchion_mt_grip_05"
+local _mt_tacax_grips = "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06"
+local _mt_ogryn_grips = "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06"
 
 -- ########
 -- For This Variant Only
@@ -55,105 +59,75 @@ mod.mt.inject_fixes(this_variant, {
     --  Why did I choose 5 blades with wildly different proportions
     --  Just love making more work for myself eh
     -- MT Combat knife grip patch
-    {   dependencies = {"owo_rapier_blade_01",
-            "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
-        },
+    {   dependencies = { "owo_rapier_blade_01", _mt_knife_grips },
         -- grip = {mesh_move = true, position = vector3_box(0, 0, 0), rotation = vector3_box(-1, 0, 0), scale = vector3_box(1, 1, 1.6), scale_node = 1},
         body = { offset = true,   position = vector3_box(0, 0.0, -0.034),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.6, 1.7) },
     },
-    {   dependencies = {"owo_rapier_blade_02",
-            "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
-        },
+    {   dependencies = { "owo_rapier_blade_02", _mt_knife_grips },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.06),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.5, 1.7) },
     },
-    {   dependencies = {"owo_rapier_blade_03",
-            "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
-        },
+    {   dependencies = { "owo_rapier_blade_03", _mt_knife_grips },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.6, 0.25, 0.6) },
     },
-    {   dependencies = {"owo_rapier_blade_04",
-            "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
-        },
+    {   dependencies = { "owo_rapier_blade_04", _mt_knife_grips },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.6, 0.3, 0.6) },
     },
-    {   dependencies = {"owo_rapier_blade_05",
-            "knife_mt_grip_01|knife_mt_grip_02|knife_mt_grip_03|knife_mt_grip_04|knife_mt_grip_05|knife_mt_grip_06|knife_mt_grip_07"
-        },
+    {   dependencies = { "owo_rapier_blade_05", _mt_knife_grips },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.04),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.3, 0.2, 0.6) },
     },
     -- MT Tacax grip patch
-    {   dependencies = {"owo_rapier_blade_01",
-            "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06",
-        },
+    {   dependencies = { "owo_rapier_blade_01", _mt_tacax_grips, },
         -- grip = {mesh_move = true, position = vector3_box(0, 0, 0), rotation = vector3_box(-1, 0, 0), scale = vector3_box(1, 1, .5), node = 1},
         body = { offset = true,   position = vector3_box(0, 0.0, -0.034),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.6, 6) },
     },
-    {   dependencies = {"owo_rapier_blade_02",
-           "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06",
-        },
+    {   dependencies = { "owo_rapier_blade_02", _mt_tacax_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.06),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.5, 6) },
     },
-    {   dependencies = {"owo_rapier_blade_03",
-            "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06",
-        },
+    {   dependencies = { "owo_rapier_blade_03", _mt_tacax_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.6, 0.25, 2) },
     },
-    {   dependencies = {"owo_rapier_blade_04",
-            "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06",
-        },
+    {   dependencies = { "owo_rapier_blade_04", _mt_tacax_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.6, 0.3, 2) },
     },
-    {   dependencies = {"owo_rapier_blade_05",
-            "hatchet_mt_grip_01|hatchet_mt_grip_02|hatchet_mt_grip_03|hatchet_mt_grip_04|hatchet_mt_grip_05|hatchet_mt_grip_06",
-        },
+    {   dependencies = { "owo_rapier_blade_05", _mt_tacax_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.04),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.3, 0.2, 2) },
     },
     -- MT Ogryn grip patch
-    {   dependencies = {"owo_rapier_blade_01",
-            "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06",
-        },
+    {   dependencies = { "owo_rapier_blade_01", _mt_ogryn_grips, },
         -- grip = {mesh_move = "both", position = vector3_box(0, 0, 0), rotation = vector3_box(-1, 0, 0), scale = vector3_box(.35, .35, .6), },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.034),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(2.7, 1.7, 5) },
     },
-    {   dependencies = {"owo_rapier_blade_02",
-           "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06",
-        },
+    {   dependencies = { "owo_rapier_blade_02", _mt_ogryn_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.06),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(2.7, 1.4, 5) },
     },
-    {   dependencies = {"owo_rapier_blade_03",
-            "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06",
-        },
+    {   dependencies = { "owo_rapier_blade_03", _mt_ogryn_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1.7, 0.9, 1.7) },
     },
-    {   dependencies = {"owo_rapier_blade_04",
-            "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06",
-        },
+    {   dependencies = { "owo_rapier_blade_04", _mt_ogryn_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1.7, 1, 1.7) },
     },
-    {   dependencies = {"owo_rapier_blade_05",
-            "ogrynbladehandle_01|ogrynbladehandle_02|ogrynbladehandle_03|ogrynbladehandle_04|ogrynbladehandle_05|ogrynbladehandle_06",
-        },
+    {   dependencies = { "owo_rapier_blade_05", _mt_ogryn_grips, },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.04),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.9, 0.85, 1.7) },
     },
     -- Generic Case
     --  Knife based
-    {   dependencies = {"owo_rapier_blade_01"},
+    {   dependencies = { "owo_rapier_blade_01" },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.034),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.6, 3) },
     },
     --      The one with the hole
-    {   dependencies = {"owo_rapier_blade_02"},
+    {   dependencies = { "owo_rapier_blade_02" },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.06),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.5, 3) },
     },
     --  Force sword based
-    {   dependencies = {"owo_rapier_blade_03"},
+    {   dependencies = { "owo_rapier_blade_03" },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(1, 0.25, 1) },
     },
     --      Fat blade
-    {   dependencies = {"owo_rapier_blade_04"},
+    {   dependencies = { "owo_rapier_blade_04" },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.00),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.6, 0.3, 1) },
     },
     --      ULTRA GIRTHY Deimos blade
-    {   dependencies = {"owo_rapier_blade_05"},
+    {   dependencies = { "owo_rapier_blade_05" },
         body = { offset = true,   position = vector3_box(0, 0.0, -0.04),   rotation = vector3_box(0, 0, 0),    scale = vector3_box(0.3, 0.2, 1) },
     },
 
