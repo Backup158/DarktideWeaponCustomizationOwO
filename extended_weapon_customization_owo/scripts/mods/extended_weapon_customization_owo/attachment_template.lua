e553f862a90f03723f2a160060377068d35365a8
LackupL58 <beekoop158+gh@gmail.com>
2025-09-13 18:11:02 -0600

    chore: attachment template for dev

---
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachment_template.lua
@@ -0,0 +1,47 @@
+-- ##############################
+-- TEMPLATES FOR CREATING ATTACHMENTS
+-- 
+function mod.KITBASH_ITEM()
+    local table_to_return = init_table_to_return("KITBASH_ITEM")
+
+    create_an_attachment(table_to_return, "LALALALALA_NAME",
+        -- Attachment
+        {   replacement_path = _item_ranged.."/muzzles/LALALALALA_NAME",
+            icon_render_unit_rotation_offset = {90, 0, 30},
+            icon_render_camera_position_offset = {-0.2, -1.75, 0.15},
+        },
+        -- Fixes
+        nil,
+        -- Kitbash
+        {   item = _item_empty_trinket, -- invisible base
+            fix = {
+                disable_in_ui = true,
+                offset = {
+                    node = 1,
+                    position = vector3_box(0.0, 0.0, 0.0),
+                    rotation = vector3_box(0.0, 0.0, 0.0),
+                    scale = vector3_box(1.0, 1.0, 1.0)
+                },
+            },
+            children = {
+                CHILD_SLOT_NAME = {
+                    item = _item_ranged.."/muzzles/autogun_rifle_ak_muzzle_03",
+                    fix = {
+                        offset = {
+                            node = 1,
+                            position = vector3_box(0, 0.0, 0.0),
+                            rotation = vector3_box(0.0, 0.0, 0.0),
+                            scale = vector3_box(1.0, 1.0, 1.0),
+                        },
+                    },
+                },
+            },
+        },
+        -- ATTACHMENT NODE 
+        -- DON'T FORGET THIS
+        "ap_muzzle_01"
+    )
+
+    return table_to_return
+
+end
