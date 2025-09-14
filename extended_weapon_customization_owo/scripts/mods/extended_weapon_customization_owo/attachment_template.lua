b521ecf847a6f9c4abc39f9211162a551921039a
LackupL58 <beekoop158+gh@gmail.com>
2025-09-13 19:13:59 -0600

    chore: non kitbash template

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachment_template.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachment_template.lua
@@ -55,3 +55,26 @@ function mod.KITBASH_ITEM()
     return table_to_return
 
 end
+
+-- For normal
+function mod.NORMAL_ITEM()
+    local table_to_return = init_table_to_return("NORMAL_ITEM")
+
+    create_an_attachment(table_to_return, "LALALALALA_NAME",
+        -- Attachment
+        {   replacement_path = _item_ranged.."/muzzles/LALALALALA_NAME",
+            icon_render_unit_rotation_offset = {90, 0, 30},
+            icon_render_camera_position_offset = {-0.2, -1.75, 0.15},
+        },
+        -- Fixes
+        nil,
+        -- Kitbash
+        nil,
+        -- ATTACHMENT NODE 
+        -- DON'T FORGET THIS
+        "ap_muzzle_01"
+    )
+
+    return table_to_return
+
+end
