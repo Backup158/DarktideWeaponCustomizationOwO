7792f1efe6496772f3dca844b6d1a5564f055c79
LackupL58 <beekoop158+gh@gmail.com>
2025-09-13 19:10:57 -0600

    feat: more weapons for suppressor

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/extended_weapon_customization_owo.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/extended_weapon_customization_owo.lua
@@ -241,7 +241,7 @@ load_mod_file("create_attachments")
 -- ################################
 -- Adding Attachments
 -- ################################
-add_attachments_to_list_of_weapons(mod.owo_suppressor(), {"autogun_p1_m1", "lasgun_p1_m1"}, "muzzle")
+add_attachments_to_list_of_weapons(mod.owo_suppressor(), {"autogun_p1_m1", "autopistol_p1_m1", "bolter_p1_m1", "boltpistol_p1_m1", "lasgun_p1_m1", "lasgun_p2_m1", "lasgun_p3_m1"}, "muzzle")
 
 -- ################################
 -- Copying to Different Marks
