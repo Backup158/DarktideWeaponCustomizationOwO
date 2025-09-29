d438b858b8ed55397b080948f7f373258ea7b928
LackupL58 <beekoop158+gh@gmail.com>
2025-09-28 23:17:46 -0600

    style: these fixes are so ass

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_slim_blade.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_slim_blade.lua
@@ -38,20 +38,13 @@ function mod.owo_slim_blade(given_slot_name, given_attachment_node)
     local attachment_group_prefix = attachment_group_name.."_"
     local table_to_return = mod.init_table_to_return(attachment_group_name)
 
-    local flat_psword_scl = vector3_box(0.35, 1.0, 1.0)
-    local flat_psword_g_scl = vector3_box(0.5, 1.0, 1.0)
-    local slim_psword_scl = vector3_box(0.35, 0.65, 1.0)
-    local slim_psword_g_scl = vector3_box(0.5, 0.65, 1.0)
-    local flat_dclaw_scl = vector3_box(0.5, 1.0, 1.0)
-    local flat_dclaw_g_scl = vector3_box(0.6, 1.0, 1.0)
-    local slim_dclaw_scl = vector3_box(0.5, 0.65, 1.0)
-    local slim_dclaw_g_scl = vector3_box(0.6, 0.65, 1.0)
     local grip_scale = vector3_box(0.5, 1.0, 1.0)
 
     local function slim_blade_attach_helper(number_string, name_to_use, base_item_address, scales_table)
         local slim_blade_name = attachment_group_prefix..name_to_use..number_string
         local fixes_to_add = nil
         if type(scales_table) == "table" and scales_table[2] then
+            -- this is going to be so ass to look at :wilted_rose:
             fixes_to_add = {
                 -- Making grip smaller
                 {   attachment_slot = "grip",
@@ -60,8 +53,8 @@ function mod.owo_slim_blade(given_slot_name, given_attachment_node)
                             has = slim_blade_name,
                         },
                         grip = {
-                            has = "power_sword_grip_01|power_sword_grip_02|power_sword_grip_03|power_sword_grip_04|power_sword_grip_05|power_sword_grip_06|power_sword_grip_ml01"
-                            --missing = "empty",
+                            --has = "power_sword_grip_01|power_sword_grip_02|power_sword_grip_03|power_sword_grip_04|power_sword_grip_05|power_sword_grip_06|power_sword_grip_ml01"
+                            missing = "empty",
                         },
                     },
                     fix = {
@@ -115,6 +108,10 @@ function mod.owo_slim_blade(given_slot_name, given_attachment_node)
     -- ------------------
     -- Power Sword Blades
     -- ------------------
+    local flat_psword_scl = vector3_box(0.35, 1.0, 1.0)
+    local flat_psword_g_scl = vector3_box(0.5, 1.0, 1.0)
+    local slim_psword_scl = vector3_box(0.35, 0.65, 1.0)
+    local slim_psword_g_scl = vector3_box(0.5, 0.65, 1.0)
     -- Flat PSword
     for_all_weapon_models(7, {4}, function(number_string)
         slim_blade_attach_helper(number_string, "flat_psword_", _item_melee.."/blades/power_sword_blade_", flat_psword_scl)
@@ -134,6 +131,10 @@ function mod.owo_slim_blade(given_slot_name, given_attachment_node)
     -- ------------------
     -- "Devil's Claw" Blades
     -- ------------------
+    local flat_dclaw_scl = vector3_box(0.5, 1.0, 1.0)
+    local flat_dclaw_g_scl = vector3_box(0.6, 1.0, 1.0)
+    local slim_dclaw_scl = vector3_box(0.5, 0.65, 1.0)
+    local slim_dclaw_g_scl = vector3_box(0.6, 0.65, 1.0)
     -- Flat dclaw
     for_all_weapon_models(7, nil, function(number_string)
         slim_blade_attach_helper(number_string, "flat_dclaw_", _item_melee.."/blades/combat_sword_blade_", flat_dclaw_scl)
@@ -150,7 +151,30 @@ function mod.owo_slim_blade(given_slot_name, given_attachment_node)
     for_all_weapon_models(7, nil, function(number_string)
         slim_blade_attach_helper(number_string, "slim_dclaw_g_", _item_melee.."/blades/combat_sword_blade_", {slim_dclaw_g_scl, grip_scale})
     end)
-    mod:info("uwu fuck you bitch")
+    -- ------------------
+    -- Heavy Sword
+    -- ------------------
+    local flat_hsword_scl = vector3_box(0.35, 1.0, 1.0)
+    local flat_hsword_g_scl = vector3_box(0.5, 1.0, 1.0)
+    local slim_hsword_scl = vector3_box(0.35, 0.65, 1.0)
+    local slim_hsword_g_scl = vector3_box(0.5, 0.65, 1.0)
+    -- Flat hsword
+    for_all_weapon_models(6, nil, function(number_string)
+        slim_blade_attach_helper(number_string, "flat_hsword_", _item_melee.."/blades/falchion_blade_", flat_hsword_scl)
+    end)
+    -- Flat hsword (Grip)
+    for_all_weapon_models(6, nil, function(number_string)
+        slim_blade_attach_helper(number_string, "flat_hsword_g_", _item_melee.."/blades/falchion_blade_", {flat_hsword_g_scl, grip_scale})
+    end)
+    -- Slim hsword
+    for_all_weapon_models(6, nil, function(number_string)
+        slim_blade_attach_helper(number_string, "slim_hsword_", _item_melee.."/blades/falchion_blade_", slim_hsword_scl)
+    end)
+    -- Slim hsword (Grip)
+    for_all_weapon_models(6, nil, function(number_string)
+        slim_blade_attach_helper(number_string, "slim_hsword_g_", _item_melee.."/blades/falchion_blade_", {slim_hsword_g_scl, grip_scale})
+    end)
+    
     --table.dump(table_to_return.fixes, "slim fixes", 6)
     return table_to_return
 
