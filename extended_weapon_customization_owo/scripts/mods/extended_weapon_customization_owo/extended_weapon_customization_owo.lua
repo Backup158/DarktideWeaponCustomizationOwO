edfb747095df2d3f18bd53a27a927cc11833718e
LackupL58 <beekoop158+gh@gmail.com>
2025-09-28 23:34:32 -0600

    feat: flashlights to a bunch

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/extended_weapon_customization_owo.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/extended_weapon_customization_owo.lua
@@ -305,7 +305,7 @@ attachments_table_for_ewc.flashlight_templates = table_clone(load_mod_file("atta
 add_attachments_to_list_of_weapons(mod.owo_suppressor(), {"autogun_p1_m1", "autopistol_p1_m1", "bolter_p1_m1", "boltpistol_p1_m1", "lasgun_p1_m1", "lasgun_p2_m1", "lasgun_p3_m1", }, "muzzle")
 add_attachments_to_list_of_weapons(mod.owo_tactical_stock(), {"autogun_p1_m1", "autopistol_p1_m1", "bolter_p1_m1", "boltpistol_p1_m1", "lasgun_p1_m1", "lasgun_p2_m1", }, "stock")
 add_attachments_to_list_of_weapons(mod.probe_for_node(), {"lasgun_p2_m1", }, "stock")
-add_attachments_to_list_of_weapons(mod.owo_flashlight(), {"lasgun_p3_m1", }, "flashlight")
+add_attachments_to_list_of_weapons(mod.owo_flashlight(), {"autogun_p1_m1", "autopistol_p1_m1", "bolter_p1_m1", "boltpistol_p1_m1", "lasgun_p1_m1", "lasgun_p2_m1", "lasgun_p3_m1", }, "flashlight")
 add_attachments_to_list_of_weapons(mod.owo_slim_blade(), {"powersword_p1_m1", "powersword_p2_m1", }, "blade")
 add_attachments_to_list_of_weapons(mod.owo_slim_blade("body", "ap_body_01"), {"combatsword_p1_m1", "combatsword_p2_m1", "combatsword_p3_m1", }, "body")
 --table.dump(mod.owo_slim_blade(), "SLIM BLADE EXAMPLE", 9)
