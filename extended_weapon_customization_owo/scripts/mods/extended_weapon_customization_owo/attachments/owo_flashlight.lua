362a5004a0222347ca21f86bb16a83592c83279c
LackupL58 <beekoop158+gh@gmail.com>
2025-09-29 23:19:47 -0600

    feat: loops for color and tempaltes

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_flashlight.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_flashlight.lua
@@ -40,26 +40,35 @@ function mod.owo_flashlight()
     local attachment_group_prefix = attachment_group_name.."_"
     local table_to_return = mod.init_table_to_return(attachment_group_name)
 
-    for_all_weapon_models(5, {"ml01"}, function(i)
-        local owo_colored_flashlight = attachment_group_prefix.."green_"..i
-        create_an_attachment(table_to_return, owo_colored_flashlight,
-            -- Attachment
-            {   replacement_path = _item_ranged.."/flashlights/"..owo_colored_flashlight,
-                icon_render_unit_rotation_offset = render_unit_rot_profile_left,
-                icon_render_camera_position_offset = render_cam_pos_profile_left,
-                flashlight_template = "green_flashlight_01",
-            },
-            -- Fixes
-            nil,
-            -- Kitbash
-            {
-                base_unit = "content/weapons/player/attachments/flashlights/flashlight_"..i.."/flashlight_"..i
-            },
-            -- ATTACHMENT NODE 
-            -- DON'T FORGET THIS
-            "ap_flashlight_01"
-        )
-    end)
+    -- needs to be manually uploaded because FUCK YOU
+    local table_of_template_endings = {"narrow", "narrow_cool", "weak", "default"}
+    local flashlight_colors = mod.flashlight_colors
+    
+    for color, _ in pairs(flashlight_colors) do
+        for template_suffix, _ in ipairs(table_of_template_endings) do
+            -- 1 for each physical model
+            for_all_weapon_models(5, {"ml01"}, function(i)
+                local owo_colored_flashlight = attachment_group_prefix..color.."_"..i
+                create_an_attachment(table_to_return, owo_colored_flashlight,
+                    -- Attachment
+                    {   replacement_path = _item_ranged.."/flashlights/"..owo_colored_flashlight,
+                        icon_render_unit_rotation_offset = render_unit_rot_profile_left,
+                        icon_render_camera_position_offset = render_cam_pos_profile_left,
+                        flashlight_template = color.."_flashlight_"..template_suffix,
+                    },
+                    -- Fixes
+                    nil,
+                    -- Kitbash
+                    {
+                        base_unit = "content/weapons/player/attachments/flashlights/flashlight_"..i.."/flashlight_"..i
+                    },
+                    -- ATTACHMENT NODE 
+                    -- DON'T FORGET THIS
+                    "ap_flashlight_01"
+                )
+            end)
+        end
+    end
     
 
     return table_to_return
