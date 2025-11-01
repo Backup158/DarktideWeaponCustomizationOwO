b298f0bca56f538643efdfc520b717f0566607aa
LackupL58 <beekoop158+gh@gmail.com>
2025-11-01 15:03:31 -0600

    chore: update template for compat

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/fixes/fixes_for_weapon_template.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/fixes/fixes_for_weapon_template.lua
@@ -16,7 +16,9 @@ local _item_minion = "content/items/weapons/minions"
 -- ###################################################################
 -- FIXES TO RETURN
 -- ###################################################################
-return {
+
+
+local custom_fixes = {
     -- Tactical Stocks
     {
         attachment_slot = "stock",
@@ -33,4 +35,33 @@ return {
             },
         },
     },
+}
+
+local custom_attachments = {
+
+}
+-- Patches for if other plugins already defined
+local ewc_ba = get_mod("extended_weapon_customization_base_additions")
+if not ewc_ba then
+    custom_attachments.flashlight = {
+        parent_slot = "barrel",
+        default_path = _item_empty_trinket,
+        fix = {
+            offset = {
+                position = vector3_box(0.015, 0.17, 0.005),
+                rotation = vector3_box(0, 0, 0),
+                scale = vector3_box(0.75, 0.75, 0.75),
+                node = 1,
+            },
+        },
+    }
+    custom_attachments.muzzle = {
+        parent_slot = "rail",
+        default_path = _item_empty_trinket,
+    }
+end
+
+return {
+    fixes = custom_fixes,
+    attachment_slots = custom_attachments,
 }
