de636b0069507c1d1e732a8efc43eb363152b612
Backup158 <beekoop158@gmail.com>
2026-03-12 18:11:36 -0500

    feat: added slim wrapped supp

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_suppressor.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_suppressor.lua
@@ -327,8 +327,8 @@ function mod.owo_suppressor(given_slot_name, given_attachment_node)
     local suppressor_wrapped_ac1_rot = vector3_box(-90, 0, 0)
     local suppressor_wrapped_ac1_sca = vector3_box(0.77, 0.6, 1.1)
     local suppressor_wrapped_ac2_pos = vector3_box(0, 0.164, 0)
-    --local suppressor_wrapped_ac1_sca_slim = vector3_box(1.1, 1.1, 1.1)
-    create_suppressor("owo_suppressor_wrapped_01", 
+    local suppressor_wrapped_ac1_sca_slim = vector3_box(0.6, 0.48, 1.1)
+    create_suppressor_and_slim("owo_suppressor_wrapped_01", 
         {
             ac1 = _item_ranged.."/muzzles/wrapped_ac1",
             ac2 = _item_ranged.."/muzzles/supp_ac_ak_muzzle_02",
@@ -339,11 +339,11 @@ function mod.owo_suppressor(given_slot_name, given_attachment_node)
             ac1_pos = suppressor_wrapped_ac1_pos,
             ac1_rot = suppressor_wrapped_ac1_rot,
             ac1_sca = suppressor_wrapped_ac1_sca,
-            --ac1_sca_slim = suppressor_wrapped_ac1_sca_slim,
+            ac1_sca_slim = suppressor_wrapped_ac1_sca_slim,
             ac2_pos = suppressor_wrapped_ac2_pos,
             ac2_rot = suppressor_metal_ac2_rot,
             ac2_sca = suppressor_metal_ac2_sca,
-            --ac2_sca_slim = suppressor_metal_ac2_sca_slim,
+            ac2_sca_slim = suppressor_metal_ac2_sca_slim,
         }
     )
 
