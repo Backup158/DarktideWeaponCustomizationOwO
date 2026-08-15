13b6826ca2ba5fa23abb0ba3bb04641eb6e5aaa2
Backup158 <beekoop158@gmail.com>
2026-08-15 16:48:40 -0500

    style: don't need to check each step anymore

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/fixes/shared_fix_requirements.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/fixes/shared_fix_requirements.lua
@@ -52,7 +52,7 @@ for i = 1, #(slim_blades_flip_list_blade) do
         for k = 1, #(slim_blades_flip_list_suffix) do
             local blade = "owo_slim_blade_"..slim_blades_flip_list_prefix[j]..slim_blades_flip_list_blade[i]..slim_blades_flip_list_suffix[k]
             slim_blades_to_flip[blade] = create_requirements_string_from_names_table(mod.all_slim_blade_names[blade])
-            info_if_debug("results of flipping nyaaaa: "..blade.."\n"..slim_blades_to_flip[blade])
+            -- info_if_debug("results of flipping nyaaaa: "..blade.."\n"..slim_blades_to_flip[blade])
             mod.all_slim_blade_names[blade] = slim_blades_to_flip[blade]
         end
     end
