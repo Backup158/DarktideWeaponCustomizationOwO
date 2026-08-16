c5368be0de00298bf2c19d9e2389dce77c3639c2
Backup158 <beekoop158@gmail.com>
2026-08-16 00:59:44 -0500

    [fix] overwrote things and made fixes apply willy nilly

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_modern_barrel.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/attachments/owo_modern_barrel.lua
@@ -95,52 +95,10 @@ function mod.owo_kalashnikov_barrel(given_slot, given_attachment_node)
     --create_kitbash_full_item(table_to_return, _item_ranged.."/barrels/owo_kalashnikov_barrel_base_to_hide", nil, "content/weapons/player/ranged/autogun_rifle_ak/attachments/barrel_02/barrel_02", current_attachment_node)
     local owo_kalash_kitbash_item_string_prefix = _item_ranged.."/barrels/owo_kalashnikov_barrel_helper_barrel_"
     
-    list_of_names_to_use.type2 = list_of_names_to_use.type2 or {}
-    local type2_short_distance = 0.268
-    local type2_super_short_distance = 0.2
-    create_kitbash_full_item(table_to_return, owo_kalash_kitbash_item_string_prefix.."squish_03", nil, "content/weapons/player/ranged/autogun_rifle_ak/attachments/barrel_03/barrel_03", current_attachment_node)
-    -- Long Barrels (3 and 4 but the bag on 4 looks wrong
-    owo_kalashnikov_barrel_helper("short", "03", nil, 
-    {
-        base_unit = "content/weapons/player/ranged/lasgun_rifle_krieg/attachments/barrel_07/barrel_07",
-        attachments = {
-            owo_barrel_we_actually_see = {
-                item = _item_ranged.."/barrels/owo_kalashnikov_barrel_helper_barrel_squish_03",
-                fix = {
-                    offset = {
-                        node = 1,
-                        position = vector3_box(0.0, -1 * type2_short_distance, 0.0),
-                        rotation = vector3_box(0.0, 0, 0.0),
-                        scale = vector3_box(1, 0.6, 1),
-                    },
-                },
-            },
-            
-        },
-    }, list_of_names_to_use.type2)
-    owo_kalashnikov_barrel_helper("super_short", "03", nil, 
-    {
-        base_unit = "content/weapons/player/ranged/lasgun_rifle_krieg/attachments/barrel_07/barrel_07",
-        attachments = {
-            owo_barrel_we_actually_see = {
-                item = owo_kalash_kitbash_item_string_prefix.."squish_03",
-                fix = {
-                    offset = {
-                        node = 1,
-                        position = vector3_box(0.0, -1 * type2_super_short_distance, 0.0),
-                        rotation = vector3_box(0.0, 0, 0.0),
-                        scale = vector3_box(1, 0.5, 1),
-                    },
-                },
-            },
-            
-        },
-    }, list_of_names_to_use.type2)
-    
     -- Short: This one is slightly longer than the other shorts
     list_of_names_to_use.type1 = list_of_names_to_use.type1 or {}
-    local type1_short_distance = 0.268
-    local type1_super_short_distance = 0.2
+    local type1_short_distance = 0.202
+    local type1_super_short_distance = 0.19
     for_all_weapon_models(8, {3, 4}, function(number_as_string)
         local owo_kitbash_helper_address = owo_kalash_kitbash_item_string_prefix.."squish_"..number_as_string
         create_kitbash_full_item(table_to_return, owo_kitbash_helper_address, nil, "content/weapons/player/ranged/autogun_rifle_ak/attachments/barrel_"..number_as_string.."/barrel_"..number_as_string, current_attachment_node)
@@ -180,16 +138,62 @@ function mod.owo_kalashnikov_barrel(given_slot, given_attachment_node)
         }, list_of_names_to_use.type1)
     end)
 
+    -- Type 2: Long Barrels (3 and 4 but the bag on 4 looks wrong
+    list_of_names_to_use.type2 = list_of_names_to_use.type2 or {}
+    local type2_short_distance = 0.268
+    local type2_super_short_distance = 0.2
+    create_kitbash_full_item(table_to_return, owo_kalash_kitbash_item_string_prefix.."squish_03", nil, "content/weapons/player/ranged/autogun_rifle_ak/attachments/barrel_03/barrel_03", current_attachment_node)
+    owo_kalashnikov_barrel_helper("short", "03", nil, 
+    {
+        base_unit = "content/weapons/player/ranged/lasgun_rifle_krieg/attachments/barrel_07/barrel_07",
+        attachments = {
+            owo_barrel_we_actually_see = {
+                item = _item_ranged.."/barrels/owo_kalashnikov_barrel_helper_barrel_squish_03",
+                fix = {
+                    offset = {
+                        node = 1,
+                        position = vector3_box(0.0, -1 * type2_short_distance, 0.0),
+                        rotation = vector3_box(0.0, 0, 0.0),
+                        scale = vector3_box(1, 0.6, 1),
+                    },
+                },
+            },
+            
+        },
+    }, list_of_names_to_use.type2)
+    owo_kalashnikov_barrel_helper("super_short", "03", nil, 
+    {
+        base_unit = "content/weapons/player/ranged/lasgun_rifle_krieg/attachments/barrel_07/barrel_07",
+        attachments = {
+            owo_barrel_we_actually_see = {
+                item = owo_kalash_kitbash_item_string_prefix.."squish_03",
+                fix = {
+                    offset = {
+                        node = 1,
+                        position = vector3_box(0.0, -1 * type2_super_short_distance, 0.0),
+                        rotation = vector3_box(0.0, 0, 0.0),
+                        scale = vector3_box(1, 0.5, 1),
+                    },
+                },
+            },
+            
+        },
+    }, list_of_names_to_use.type2)
+
     -- Making list of all attachments global
     if all_these_attachments then
         mod.all_owo_kalashnikov_barrel_names = {}
+        mod.all_owo_kalashnikov_barrel_names.type1 = mod.all_owo_kalashnikov_barrel_names.type1 or {}
         mod.all_owo_kalashnikov_barrel_names.type1.short = mod.create_requirements_string_from_names_table(all_these_attachments.type1.short)
         mod.all_owo_kalashnikov_barrel_names.type1.super_short = mod.create_requirements_string_from_names_table(all_these_attachments.type1.super_short)
-        mod.all_owo_kalashnikov_barrel_names.type1 = mod.create_requirements_string_from_names_table(amod.all_owo_kalashnikov_barrel_names.type1)
+        mod.all_owo_kalashnikov_barrel_names.type1.all = mod.create_requirements_string_from_names_table(mod.all_owo_kalashnikov_barrel_names.type1)
+        mod.all_owo_kalashnikov_barrel_names.type2 = mod.all_owo_kalashnikov_barrel_names.type2 or {}
         mod.all_owo_kalashnikov_barrel_names.type2.short = mod.create_requirements_string_from_names_table(all_these_attachments.type2.short)
         mod.all_owo_kalashnikov_barrel_names.type2.super_short = mod.create_requirements_string_from_names_table(all_these_attachments.type2.super_short)
-        mod.all_owo_kalashnikov_barrel_names.type2 = mod.create_requirements_string_from_names_table(amod.all_owo_kalashnikov_barrel_names.type2)
-        mod.all_owo_kalashnikov_barrel_names.all = mod.all_owo_kalashnikov_barrel_names.type1.."|"..mod.all_owo_kalashnikov_barrel_names.type2
+        mod.all_owo_kalashnikov_barrel_names.type2.all = mod.create_requirements_string_from_names_table(mod.all_owo_kalashnikov_barrel_names.type2)
+        mod.all_owo_kalashnikov_barrel_names.all = mod.all_owo_kalashnikov_barrel_names.type1.all.."|"..mod.all_owo_kalashnikov_barrel_names.type2.all
+        --table.dump(all_these_attachments, "uwu all these attachments kalashnikov", 15)
+        --table.dump(mod.all_owo_kalashnikov_barrel_names, "uwu global var these attachments kalashnikov", 15)
     end
 
     -- These fixes are used by multiple barrels
@@ -212,35 +216,6 @@ function mod.owo_kalashnikov_barrel(given_slot, given_attachment_node)
         }
     )
     -- Barrel
-    --  Type 2 (Longer)
-    table_insert(table_to_return.fixes, 
-    {
-        attachment_slot = "barrel",
-        requirements = {
-            barrel = {
-                has = mod.all_owo_kalashnikov_barrel_names.type2.short,
-            },
-        },
-        fix = {
-            offset = {
-                position = vector3_box(0.0, type2_short_distance, -0.03),
-            },
-        },
-    })
-    table_insert(table_to_return.fixes, 
-    {
-        attachment_slot = "barrel",
-        requirements = {
-            barrel = {
-                has = mod.all_owo_kalashnikov_barrel_names.type2.super_short,
-            },
-        },
-        fix = {
-            offset = {
-                position = vector3_box(0.0, type2_super_short_distance, -0.03),
-            },
-        },
-    })
     --  Type 1 (Shorter)
     table_insert(table_to_return.fixes, 
     {
@@ -270,6 +245,37 @@ function mod.owo_kalashnikov_barrel(given_slot, given_attachment_node)
             },
         },
     })
+    --  Type 2 (Longer)
+    table_insert(table_to_return.fixes, 
+    {
+        attachment_slot = "barrel",
+        requirements = {
+            barrel = {
+                has = mod.all_owo_kalashnikov_barrel_names.type2.short,
+            },
+        },
+        fix = {
+            offset = {
+                position = vector3_box(0.0, type2_short_distance, -0.03),
+            },
+        },
+    })
+    table_insert(table_to_return.fixes, 
+    {
+        attachment_slot = "barrel",
+        requirements = {
+            barrel = {
+                has = mod.all_owo_kalashnikov_barrel_names.type2.super_short,
+            },
+        },
+        fix = {
+            offset = {
+                position = vector3_box(0.0, type2_super_short_distance, -0.03),
+            },
+        },
+    })
+
+    --table.dump(table_to_return.fixes, "uwu fixes from kalashnikov", 15)
 
     return table_to_return
 
