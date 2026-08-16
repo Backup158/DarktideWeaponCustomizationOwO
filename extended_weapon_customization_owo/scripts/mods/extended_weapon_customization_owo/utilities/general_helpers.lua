2df50a5be67156ae27a866c275e9372b893bb985
Backup158 <beekoop158@gmail.com>
2026-08-15 22:24:05 -0500

    [fix] in case no number is needed

--- extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/utilities/general_helpers.lua
+++ extended_weapon_customization_owo/scripts/mods/extended_weapon_customization_owo/utilities/general_helpers.lua
@@ -213,9 +213,14 @@ function mod.localize_attachment_triple_threat(attachment_group_name, attachment
 
     local attachment_start = mod:Localize("loc_"..attachment_group_name)
     local attachment_end = mod:Localize("loc_"..attachment_group_name.."_"..attachment_suffix)
+
+    local attachment_combination = attachment_start.." - "..attachment_end
+    if number_string then
+        attachment_combination = attachment_combination.." "..number_string
+    end
     mod:add_global_localize_strings({
         ["loc_"..attachment_name] = {
-            en = string_format("%s - %s %s", attachment_start, attachment_end, number_string)
+            en = attachment_combination
         },
     })
 end
